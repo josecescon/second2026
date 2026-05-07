fb4361ab48be44c82de409a51a6ea130a25f9ab9
josecescon <jose.cescon@escola.pr.gov.br>
2026-05-07 11:07:21 -0300

    Add files via upload

--- produtos.js
+++ produtos.js
@@ -10,16 +10,18 @@ async function sigma() {
             const card = document.createElement('div');
             card.className = 'card';
             card.innerHTML = `
-                <img src="imagem/${item.imagem}" alt = "Foto de ${item.nome}">
-                <h3></h3>
+                <img src="imagem/${item.imagem}" alt = "Foto de ${item.nome}" class="produto">
+                <h1></h1>
                 <p></p>
+                <h2 ${item.preco}></h2>
                 <a href="${item.endereco}">
                  <button type="button">Saiba mais </button>
                 </a>
+                <br><br>
             `;
-            card.querySelector('h3').textContent = item.nome;
+            card.querySelector('h1').textContent = item.nome;
             card.querySelector('p').textContent = item.descricao;
-
+            card.querySelector('h2').textContent = item.preco;
             destino.appendChild(card);
         });
     }catch (error){
